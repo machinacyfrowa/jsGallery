3bbfe1ed833ececf26948b2e62346483b319332c
Paweł Zawadzki <pawel.zawadzki2@teb.edu.pl>
2024-05-17 10:37:12 +0200

    "Updated index.html, script.js, and style.css files: changed image source in index.html, added new functions and event listeners in script.js, and modified CSS styles in style.css."

--- script.js
+++ script.js
@@ -1,3 +1,57 @@
+function getImageUrls() {
+    const element = document.getElementById('thumbnailRoll');
+    const images = element.getElementsByTagName('img');
+    const urls = [];
+
+    for (let i = 0; i < images.length; i++) {
+        //pobiera pełną ścieżkę do pliku
+        const url = images[i].src;
+        //dzieli ścieżkę na każdym znaku / i bierze tyko ostatni element
+        //(nazwę pliku)
+        const path = url.split('/').pop();
+        urls.push(path);
+    }
+
+    return urls;
+}
+function setFirstPicture() {
+    const element = document.querySelector("#bigThumbnail img");
+    const images = getImageUrls();
+    element.src = "img/" + images[0];
+}
+function nextPicture() {
+    //lista wszystkich zdjec
+    const urlList = getImageUrls();
+    //pobierz url obecnego zdjecia
+    const currentUrl = document.querySelector("#bigThumbnail img").src;
+    //znajdz indeks zdjecia w liscie
+    let currentIndex = urlList.indexOf(currentUrl.split("/").pop());
+    if(currentIndex == urlList.length - 1) {
+        //obecne zdjecie jest ostatnim na liscie - wróć na początek listy
+        currentIndex = -1;
+    } 
+    //znajdz następne zdjęcie na liscie
+    let nextPictureUrl = "img/" + urlList[currentIndex+1];
+    //podmien url zdjecia
+    document.querySelector("#bigThumbnail img").src = nextPictureUrl;
+}
+function prevPicture() {
+    //lista wszystkich zdjec
+    const urlList = getImageUrls();
+    //pobierz url obecnego zdjecia
+    const currentUrl = document.querySelector("#bigThumbnail img").src;
+    //znajdz indeks zdjecia w liscie
+    let currentIndex = urlList.indexOf(currentUrl.split("/").pop());
+    if(currentIndex == 0) {
+        //obecne zdjecie jest pierwszym na liście - wróć na koniec
+        currentIndex = urlList.length;
+    } 
+    //znajdz następne zdjęcie na liscie
+    let nextPictureUrl = "img/" + urlList[currentIndex-1];
+    //podmien url zdjecia
+    document.querySelector("#bigThumbnail img").src = nextPictureUrl;
+}
+
 function moveRollRight() {
     // Move the roll to the right
     // element rolki z miniaturami
@@ -26,3 +80,8 @@ function moveRollLeft() {
 //zdarzenie kliknięcia strzałki od miniaturek w prawo
 document.getElementById('rightArrowSmall').addEventListener('click', moveRollRight);
 document.getElementById('leftArrowSmall').addEventListener('click', moveRollLeft);
+
+document.getElementById('rightArrow').addEventListener('click', nextPicture);
+document.getElementById('leftArrow').addEventListener('click', prevPicture);
+
+window.addEventListener('load', setFirstPicture);
